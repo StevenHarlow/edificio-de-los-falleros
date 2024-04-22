4dfcdd9491baca13a6c960e9e1246ef61c43e817
Steven Harlow <stharlow@gmail.com>
2024-04-22 15:00:52 +0200

    the copy at least

---
+++ app/lib/contentful.ts
@@ -0,0 +1,15 @@
+import { IStaticPagesFields } from "@/@types/generated/contentful";
+import * as contentful from "contentful";
+
+export const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID || "",
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+  host: process.env.CONTENTFUL_HOST || "cdn.contentful.com",
+});
+
+export type StaticPageEntrySkeleton = {
+  contentTypeId: "staticPages";
+  fields: IStaticPagesFields;
+};
+
+export type StaticPageEntry = contentful.Entry<StaticPageEntrySkeleton>;
