b30467169725d5b9ec32419484f245b676be1024
Steven Harlow <stharlow@gmail.com>
2024-04-25 00:33:46 +0200

    fix typo

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="fixed z-1000 w-full flex items-center justify-center h-16 gap-20 px-10 bg-black text-white">
-      <a href="/">Incio</a>
+      <a href="/">Inicio</a>
       <a href="/vecinos">Vecinos</a>
     </nav>
   );
